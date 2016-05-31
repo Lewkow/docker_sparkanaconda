13b53b22660290b26f271e56709add49de709701
Nicholas Lewkow <nicholas.lewkow@mheducation.com>
2016-05-31 14:21:13 -0400

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 8888:8888 -m 4g --cpu-shares=2 -v $(pwd)/jup:/root/jup lewkow/sparkanaconda:latest sh -c "pyspark"
+docker run -d -p 8888:8888 -m 4g --cpu-shares=2 -v $(pwd)/jup:/root/jup sparkanaconda sh -c "pyspark"
